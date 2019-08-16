8fae9bd0c9574f9d235d885e4746a43ee8d24a69
Kathleen Leung <kathleenleungky@gmail.com>
2019-08-16 13:35:46 -0700

    added Bands in Town concert function and tested out OMDB error msgs when user enter invalid input

--- liri.js
+++ liri.js
@@ -19,7 +19,7 @@ var userInput = process.argv.slice(3).join(" ");
 console.log(userInput);
 var output;
 
-// Function that display OMDB Movie Info output
+// Function that display OMDB Movie Info search
 function showMovie(){
     var queryURL = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
     // console.log(queryURL);
@@ -33,10 +33,11 @@ function showMovie(){
             console.log(output);
 
         }).catch(function(error) {
-            console.log("enter Error");
+            // console.log(error);
             if (error.response) {
                 // The request was made and the server responded with a status code
                 // that falls out of the range of 2xx
+                // console.log("1");
                 console.log("---------------Data---------------");
                 console.log(error.response.data);
                 console.log("---------------Status---------------");
@@ -46,11 +47,15 @@ function showMovie(){
             } else if (error.request) {
                 // The request was made but no response was received
                 // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                // console.log("2");
                 console.log(error.request);
             } else {
                 // Something happened in setting up the request that triggered an Error
-                console.log("Error", error.message);
+                // console.log("3");
+                console.log("\nError", error.message);
+                console.log(chalk.red("Please enter a valid movie!\n"));
             }
+            // console.log("4");
             console.log(error.config);
         });
     }else{
@@ -59,21 +64,27 @@ function showMovie(){
             var movie = response.data;
             // console.log(response);
             // console.log(response.data.Ratings);
-            var title = movie.Title;
-            var year = movie.Year;
-            var imdbRating = movie.Ratings[0].Value;
-            var rottenTomatoes = movie.Ratings[1].Value;
-            // var country = movie.Country.split(", ").join("\n");
-            var country = movie.Country;
-            // var language = movie.Language.split(", ").join("\n");
-            var language = movie.Language;
-            var plot = movie.Plot;
-            var actors = movie.Actors; 
 
-            output = chalk.red(`Seems like you did not enter a movie!\n`) + `It's okay, just remember to enter it next time!\n-------------------------------------------\nHere's a recommendation for you: (It's on Netflix!)\n\n` + chalk.yellow(`Movie Title: `) + `${title}\n` + chalk.yellow(`Year: `) + `${year}\n` + chalk.yellow(`IMDB Rating: `) + `${imdbRating}\n` + chalk.yellow(`Rotten Tomatoes Rating: `) + `${rottenTomatoes}\n` + chalk.yellow(`Country: `) + `${country}\n` + chalk.yellow(`Language: `) + `${language}\n` + chalk.yellow(`Plot Summary: `) + `${plot}\n` + chalk.yellow(`Actors: `) + `${actors}\n`
+            output = chalk.red(`Seems like you forgot to enter a movie!\n`) + `It's okay, just remember to enter it next time!\n-------------------------------------------\nHere's a recommendation for you: (It's on Netflix!)\n\n` + chalk.yellow(`Movie Title: `) + `${movie.Title}\n` + chalk.yellow(`Year: `) + `${movie.Year}\n` + chalk.yellow(`IMDB Rating: `) + `${movie.Ratings[0].Value}\n` + chalk.yellow(`Rotten Tomatoes Rating: `) + `${movie.Ratings[1].Value}\n` + chalk.yellow(`Country: `) + `${movie.Country}\n` + chalk.yellow(`Language: `) + `${movie.Language}\n` + chalk.yellow(`Plot Summary: `) + `${movie.Plot}\n` + chalk.yellow(`Actors: `) + `${movie.Actors}\n`
 
             console.log(output);
         })
+    }
+}
+
+// function for Bands in Town Concert Info search
+function showConcert(){
+    if(userInput !== ""){
+        var queryURL = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp"
+        axios.get(queryURL).then(function(response){
+            var concert = response.data;
+            console.log(response.data);
+            if(concert.length === 0){
+                console.log(`\nSorry! There are no upcoming events for `+ chalk.green(`${userInput}.`) + `\nThey are currently not on tour.\n\n-------------------------------------------\n`)
+            }
+        })
+    }else{
+        console.log(`\nSeems like you forgot to enter an artist or band. Please try again!\n\n-------------------------------------------\n`)
     }
 }
 
@@ -82,9 +93,10 @@ switch(command){
     case "movie-this":
         return showMovie();
     case "concert-this":
+        return showConcert();
     case "spotify-this-song":
     case "do-what-it-says":
     default:
-        console.log(`\n----------------------------------\n` + chalk.red(`\nPlease enter a valid command from the following four:\n`) + `1) movie-this\n2) concert-this\n3) spotify-this-song\n4) do-what-it-says\n`);
+        console.log(`\n-------------------------------------------\n` + chalk.red(`\nPlease enter a valid command from the following four:\n`) + `1) movie-this\n2) concert-this\n3) spotify-this-song\n4) do-what-it-says\n`);
         return false; 
 }
