35279d7f33005c5266332a7c340abd28d7ab4e81
Kathleen Leung <kathleenleungky@gmail.com>
2019-08-16 21:24:06 -0700

    added if user didnt enter a song in spotify function

--- liri.js
+++ liri.js
@@ -68,7 +68,7 @@ function showMovie(){
             output = chalk.red(`Seems like you forgot to enter a movie!\n`) + `It's okay, just remember to enter it next time!\n-------------------------------------------\nHere's a recommendation for you: (It's on Netflix!)\n\n` + chalk.yellow(`Movie Title: `) + `${movie.Title}\n` + chalk.yellow(`Year: `) + `${movie.Year}\n` + chalk.yellow(`IMDB Rating: `) + `${movie.Ratings[0].Value}\n` + chalk.yellow(`Rotten Tomatoes Rating: `) + `${movie.Ratings[1].Value}\n` + chalk.yellow(`Country: `) + `${movie.Country}\n` + chalk.yellow(`Language: `) + `${movie.Language}\n` + chalk.yellow(`Plot Summary: `) + `${movie.Plot}\n` + chalk.yellow(`Actors: `) + `${movie.Actors}\n`
 
             console.log(output);
-        })
+        });
     }
 }
 
@@ -78,28 +78,51 @@ function showConcert(){
         var queryURL = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp"
         axios.get(queryURL).then(function(response){
             var concert = response.data;
-            console.log(response.data);
-            console.log(concert.length);
+            // console.log(response.data);
+            // console.log(concert.length);
             if(concert.length === 0){
                 console.log(`\nSorry! There are no upcoming events for `+ chalk.green(`${userInput}`) + `.\nThey are currently not on tour.\n\n-------------------------------------------\n`)
             }else{
+                console.log(`\nThere are ` + chalk.yellow(concert.length) + ` upcoming events for ` + chalk.green(userInput) + ': ');
+
                 for(var i = 0; i < concert.length; i++){
                     // Time Converter
                     var date = concert[i].datetime;
                     var convertedDate = moment(date, 'YYYY-MM-DDTHH:mm:ss');
                     // console.log(convertedDate);
 
-                    output = `\n-------------------------------------------\n\nUpcoming events for ` + chalk.green(userInput) + ': \n' + chalk.yellow(`Venue Name: `) + `${concert[i].venue.name}\n` + chalk.yellow(`Venue Location: `) + `${concert[i].country}, ${concert[i].city}\n` + chalk.yellow(`Event Date: `) + convertedDate.format("MM/DD/YY hh:mm A") + `\n` 
+                    output = `\n-------------------------------------------\n\n` + chalk.yellow(`Venue Name: `) + `${concert[i].venue.name}\n` + chalk.yellow(`Venue Location: `) + `${concert[i].venue.country}, ${concert[i].venue.city}\n` + chalk.yellow(`Event Date: `) + convertedDate.format("MM/DD/YY hh:mm A") + `\n` 
 
                     console.log(output);
                 }
             }
         })
     }else{
-        
+
         output = `\nSeems like you forgot to enter an artist or band. Please try again!\n\n-------------------------------------------\n`
 
         console.log(output);
+    }
+}
+
+// function for Spotify Song Info search
+function showSong(){
+    if(userInput !== ""){
+
+    }else{
+        spotify.search({type: 'track', query: 'The Sign'}, function(err, data){
+            if (err){
+                return console.log('Error occurred: ' + err);
+            }
+            var song = data.tracks;
+
+            output = chalk.red(`Seems like you forgot to enter a song!`) + `\nIt's okay, just remember to enter it next time!\n-------------------------------------------\nHere's a recommendation for you: \n\n` + chalk.yellow(`Artist(s): `) + `${song.items[4].artists[0].name}\n` + chalk.yellow(`Song Name: `) + `${song.items[4].name}\n` + chalk.yellow(`Spotify Preview Link: `) + `${song.items[4].preview_url}\n` + chalk.yellow(`Song Album: `) + `${song.items[4].album.name}\n`
+            // console.log(data);
+            // console.log(data.tracks.items[4]);
+            // console.log("\n");
+
+            console.log(output);
+        });
     }
 }
 
@@ -110,6 +133,7 @@ switch(command){
     case "concert-this":
         return showConcert();
     case "spotify-this-song":
+        return showSong();
     case "do-what-it-says":
     default:
         console.log(`\n-------------------------------------------\n` + chalk.red(`\nPlease enter a valid command from the following four:\n`) + `1) movie-this\n2) concert-this\n3) spotify-this-song\n4) do-what-it-says\n`);
